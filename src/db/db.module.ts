ffe24f888b5929d80be505d1530eda6e6996a3af
MohdDawoodAt <m0799236313@gmail.com>
2024-12-15 14:12:12 +0300

    "created db module"

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
 import { DbService } from './db.service';
 import { ConfigService } from '@nestjs/config';
-
+import { Pool } from 'pg';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { AdminSchema } from './schemas/admin.schema';
+import { SongSchema } from 'src/songs/schemas/song.schema';
+export const DRIZZLE = Symbol('drizzle-connection');
 @Module({
-  providers: [DbService, ConfigService],
+  providers: [
+    {
+      provide: DRIZZLE,
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        // const dbUrl = configService.get<string>('pgUrl');
+        const pool = new Pool({
+          // connectionString: dbUrl,
+          user: configService.get<string>('POSTGRES_USER'),
+          host: configService.get<string>('POSTGRES_HOST'),
+          database: configService.get<string>('POSTGRES_DB'),
+          password: configService.get<string>('POSTGRES_PASSWORD'),
+          port: configService.get<number>('POSTGRES_PORT'),
+        });
+        return drizzle(pool, {
+          schema: {
+            Admin: AdminSchema,
+            Song: SongSchema,
+          },
+        });
+      },
+    },
+  ],
   exports: [DbService],
 })
 export class DbModule {
